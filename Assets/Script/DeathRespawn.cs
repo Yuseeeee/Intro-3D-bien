d5f104889c985bd94bb3e20edd9c10276e672f03
Yuseeeee <49379138@est.ort.edu.ar>
2025-05-15 10:39:07 -0300

    a

--- Assets/Script/DeathRespawn.cs
+++ Assets/Script/DeathRespawn.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DeathRespawn : MonoBehaviour
 {
+    public float limiteMuerte = -12;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class DeathRespawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < limiteMuerte)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPSParkour");
+        }
     }
 }
