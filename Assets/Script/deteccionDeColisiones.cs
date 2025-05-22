c2d84567fb614f3e5ac7c21665e99f569dc77c25
Yuseeeee <49379138@est.ort.edu.ar>
2025-05-22 10:44:15 -0300

    a

---
+++ Assets/Script/deteccionDeColisiones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        if(col.gameObject.name == "Player")
+        Destroy(gameObject);
+    }
+}
