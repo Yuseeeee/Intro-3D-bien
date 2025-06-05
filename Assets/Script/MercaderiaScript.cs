3150b07ce51793ae1463a1ed72689f3232627906
Yuseeeee <49379138@est.ort.edu.ar>
2025-06-05 10:45:55 -0300

    a

--- Assets/Script/MercaderiaScript.cs
+++ Assets/Script/MercaderiaScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MercaderiaScript : MonoBehaviour
 {
-    public int scorePoints;
+    public int scorePoints = 0;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -5,15 +5,21 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI txtScore;
+    public TextMeshProUGUI txtVida;
     // Start is called before the first frame update
     void Start()
     {
-        txtScore.text = "0";
+        txtScore.text = "0" + " puntos";
+        txtVida.text = "100 HP";
     }
 
     // Update is called once per frame
     public void UpdateScore(int score)
     {
         txtScore.text = score.ToString();
+    }
+    public void Updatehealth(int health)
+    {
+        txtVida.text = health.ToString();
     }
 }
