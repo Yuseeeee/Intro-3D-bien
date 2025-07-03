fadf30c8ad3af3ea74d02bc69d0d32ba39351c0c
Yuseeeee <49379138@est.ort.edu.ar>
2025-07-03 10:42:08 -0300

    a

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+
+{
+    public GameObject UIInteractionMessage
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIInteractionMessage setActive(false)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+Void Start
+{
+}
+
+
